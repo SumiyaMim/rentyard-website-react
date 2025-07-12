bf52785c355ad7a822de50296bdced5d8c6ced5d
SumiyaMim <sumiyaislam1093@gmail.com>
2025-07-12 21:06:20 +0600

    add information page

--- src/pages/Information.jsx
+++ src/pages/Information.jsx
@@ -2,11 +2,13 @@ import { useLocation } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 const Information = () => {
+    
   const location = useLocation();
   const propertyTitle = location.state?.propertyTitle || 'Property';
 
   return (
     <div className='flex flex-col min-h-screen bg-white'>
+      {/* information section */}
       <div className="px-8 md:px-10 lg:px-14 py-10 flex-1 overflow-hidden">
         <h2 className='text-lg font-semibold text-dark mb-5'>{propertyTitle} Information</h2>
       </div>
