7b4bdbd5310f0f8793fa87a8e78179ced46b7f2a
SumiyaMim <sumiyaislam1093@gmail.com>
2025-07-12 21:05:24 +0600

    update button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
 const Footer = ({ backText, buttonText, disabled = false, onClick = () => {}, }) => {
+
+  const navigate = useNavigate();
+
   return (
     <div className="border-t-2 border-secondary px-8 md:px-10 lg:px-14 py-5 flex justify-between items-center bg-white">
-      <h5 className="text-dark font-semibold underline underline-offset-[2px] cursor-pointer">
+      <h5 onClick={() => navigate(-1)} className="text-dark font-semibold underline underline-offset-[2px] cursor-pointer">
         {backText}
       </h5>
       <button
